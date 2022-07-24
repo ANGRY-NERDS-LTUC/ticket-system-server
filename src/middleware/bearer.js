1217e7bf62fa974a74719971a84e81cd68ca4dc7
BahaaNimer <bahaanimer97@gmail.com>
2022-07-25 02:03:01 +0300

    fixing the middlewares

--- src/middleware/bearer.js
+++ src/middleware/bearer.js
@@ -1,16 +1,49 @@
 'use strict';
-const Users = require("../models/users_model/users.model");
+
+const { Users } = require("../models/models.connection");
+const { Companies } = require("../models/models.connection");
+
+
 function bearer(req, res, next) {
     if (req.headers.authorization) {
         const bearerToken = req.headers.authorization.split(" ")[1];
-        Users.authenticateBearer(bearerToken)
-            .then((userData) => {
-                req.user = userData;
-                next();
-            })
-            .catch(() => {
-                next("Invalid Token");
-            })
+        if (Users) {
+            Users.findOne({
+                where: {
+                    token: bearerToken
+                }
+            }).then(user => {
+                if (user) {
+                    req.user = user;
+                    next();
+                } else {
+                    res.status(401).send({
+                        message: "Invalid token"
+                    });
+                }
+            });
+        }
+        if (Companies) {
+            Companies.findOne({
+                where: {
+                    token: bearerToken
+                }
+            }).then(company => {
+                if (company) {
+                    req.company = company;
+                    next();
+                } else {
+                    res.status(401).send({
+                        message: "Invalid token"
+                    });
+                }
+            }
+            );
+        }
+    } else {
+        res.status(401).send({
+            message: "No token provided"
+        });
     }
 }
 
