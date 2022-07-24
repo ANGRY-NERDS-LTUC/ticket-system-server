b34ffb2340ee34f77a2e9f8b05a82c564f30c7b3
BahaaNimer <bahaanimer97@gmail.com>
2022-07-24 10:23:51 +0300

    need refactoring

--- src/models/users_model/company.model.js
+++ src/models/users_model/company.model.js
@@ -1,82 +1,82 @@
-'use strict';
+// 'use strict';
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const SECRET = process.env.SECRET || 'secret';
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const SECRET = process.env.SECRET || 'secret';
 
-const companyModel = (sequelize, DataTypes) => {
-  const company = sequelize.define('companies', {
-    username: {
-      type: DataTypes.STRING,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      required: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-      required: true,
-      unique: true,
-    },
-    role: {
-      type: DataTypes.ENUM('service_provider', 'admin'),
-      required: true,
-      defaultValue: 'service_provider',
-    },
-    token: {
-      type: DataTypes.VIRTUAL,
-      get() {
-        return jwt.sign({ username: this.username }, SECRET);
-      },
-      set(value) {
-        let token = jwt.sign(value, SECRET);
-        return token;
-      }
-    },
-    capabilities: {
-      type: DataTypes.VIRTUAL,
-      get() {
-        const acl = {
-          admin: ['read', 'create', 'update', 'delete'],
-          service_provider: ['read', 'create', 'update'],
-        };
-        return acl[this.role];
-      }
-    }
-  });
+// const companyModel = (sequelize, DataTypes) => {
+//   const company = sequelize.define('companies', {
+//     username: {
+//       type: DataTypes.STRING,
+//       required: true,
+//       unique: true,
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       required: true,
+//     },
+//     email: {
+//       type: DataTypes.STRING,
+//       required: true,
+//       unique: true,
+//     },
+//     role: {
+//       type: DataTypes.ENUM('service_provider', 'admin'),
+//       required: true,
+//       defaultValue: 'service_provider',
+//     },
+//     token: {
+//       type: DataTypes.VIRTUAL,
+//       get() {
+//         return jwt.sign({ username: this.username }, SECRET);
+//       },
+//       set(value) {
+//         let token = jwt.sign(value, SECRET);
+//         return token;
+//       }
+//     },
+//     capabilities: {
+//       type: DataTypes.VIRTUAL,
+//       get() {
+//         const acl = {
+//           admin: ['read', 'create', 'update', 'delete'],
+//           service_provider: ['read', 'create', 'update'],
+//         };
+//         return acl[this.role];
+//       }
+//     }
+//   });
 
-  company.beforeCreate(async (user) => {
-    let hashedPassword = await bcrypt.hash(user.password, 10);
-    user.password = hashedPassword;
-  });
+//   company.beforeCreate(async (user) => {
+//     let hashedPassword = await bcrypt.hash(user.password, 10);
+//     user.password = hashedPassword;
+//   });
 
-  company.authenticateBasic = async (username, password) => {
-    let user = await user.findOne({ where: { username: username } });
-    if (!user) {
-      return 'Invalid username';
-    }
-    let passwordIsValid = await bcrypt.compare(password, user.password);
-    if (!passwordIsValid) {
-      return 'Invalid password';
-    }
-    return user;
-  };
+//   company.authenticateBasic = async (username, password) => {
+//     let user = await user.findOne({ where: { username: username } });
+//     if (!user) {
+//       return 'Invalid username';
+//     }
+//     let passwordIsValid = await bcrypt.compare(password, user.password);
+//     if (!passwordIsValid) {
+//       return 'Invalid password';
+//     }
+//     return user;
+//   };
 
-  company.authenticateToken = async (token) => {
-    try {
-      const parsedToken = jwt.verify(token, SECRET);
-      const user = await user.findOne({ where: { username: parsedToken.username } });
-      if (!user) {
-        return 'Invalid token';
-      }
-      return user;
-    } catch (err) {
-      return 'Invalid token';
-    }
-  };
+//   company.authenticateToken = async (token) => {
+//     try {
+//       const parsedToken = jwt.verify(token, SECRET);
+//       const user = await user.findOne({ where: { username: parsedToken.username } });
+//       if (!user) {
+//         return 'Invalid token';
+//       }
+//       return user;
+//     } catch (err) {
+//       return 'Invalid token';
+//     }
+//   };
 
-  return company;
-}
-module.exports = companyModel;
+//   return company;
+// }
+// module.exports = companyModel;
--- src/models/users_model/users.model.js
+++ src/models/users_model/users.model.js
@@ -1,83 +1,83 @@
-'use strict';
+// 'use strict';
 
-require('dotenv').config();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const SECRET = process.env.SECRET || 'secret';
+// require('dotenv').config();
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const SECRET = process.env.SECRET || 'secret';
 
-const userModel = (sequelize, DataTypes) => {
-  const users = sequelize.define('users', {
-    username: {
-      type: DataTypes.STRING,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      required: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-      required: true,
-      unique: true,
-    },
-    role: {
-      type: DataTypes.ENUM('admin', 'client'),
-      required: true,
-      defaultValue: 'client',
-    },
-    token: {
-      type: DataTypes.VIRTUAL,
-      get() {
-        return jwt.sign({ username: this.username }, SECRET);
-      },
-      set(value) {
-        let token = jwt.sign(value, SECRET);
-        return token;
-      }
-    },
-    capabilities: {
-      type: DataTypes.VIRTUAL,
-      get() {
-        const acl = {
-          admin: ['read', 'create', 'update', 'delete'],
-          user: ['read'],
-        };
-        return acl[this.role];
-      }
-    }
-  });
+// const userModel = (sequelize, DataTypes) => {
+//   const users = sequelize.define('users', {
+//     username: {
+//       type: DataTypes.STRING,
+//       required: true,
+//       unique: true,
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       required: true,
+//     },
+//     email: {
+//       type: DataTypes.STRING,
+//       required: true,
+//       unique: true,
+//     },
+//     role: {
+//       type: DataTypes.ENUM('admin', 'client'),
+//       required: true,
+//       defaultValue: 'client',
+//     },
+//     token: {
+//       type: DataTypes.VIRTUAL,
+//       get() {
+//         return jwt.sign({ username: this.username }, SECRET);
+//       },
+//       set(value) {
+//         let token = jwt.sign(value, SECRET);
+//         return token;
+//       }
+//     },
+//     capabilities: {
+//       type: DataTypes.VIRTUAL,
+//       get() {
+//         const acl = {
+//           admin: ['read', 'create', 'update', 'delete'],
+//           user: ['read'],
+//         };
+//         return acl[this.role];
+//       }
+//     }
+//   });
 
-  users.beforeCreate(async (user) => {
-    let hashedPassword = await bcrypt.hash(user.password, 10);
-    user.password = hashedPassword;
-  });
+//   users.beforeCreate(async (user) => {
+//     let hashedPassword = await bcrypt.hash(user.password, 10);
+//     user.password = hashedPassword;
+//   });
 
-  users.authenticateBasic = async (username, password) => {
-    let user = await user.findOne({ where: { username: username } });
-    if (!user) {
-      return 'Invalid username';
-    }
-    let passwordIsValid = await bcrypt.compare(password, user.password);
-    if (!passwordIsValid) {
-      return 'Invalid password';
-    }
-    return user;
-  };
+//   users.authenticateBasic = async (username, password) => {
+//     let user = await user.findOne({ where: { username: username } });
+//     if (!user) {
+//       return 'Invalid username';
+//     }
+//     let passwordIsValid = await bcrypt.compare(password, user.password);
+//     if (!passwordIsValid) {
+//       return 'Invalid password';
+//     }
+//     return user;
+//   };
 
-  users.authenticateToken = async (token) => {
-    try {
-      const parsedToken = jwt.verify(token, SECRET);
-      const user = await user.findOne({ where: { username: parsedToken.username } });
-      if (!user) {
-        return 'Invalid token';
-      }
-      return user;
-    } catch (err) {
-      return 'Invalid token';
-    }
-  };
+//   users.authenticateToken = async (token) => {
+//     try {
+//       const parsedToken = jwt.verify(token, SECRET);
+//       const user = await user.findOne({ where: { username: parsedToken.username } });
+//       if (!user) {
+//         return 'Invalid token';
+//       }
+//       return user;
+//     } catch (err) {
+//       return 'Invalid token';
+//     }
+//   };
 
-  return users;
-}
-module.exports = userModel;
+//   return users;
+// }
+// module.exports = userModel;
