d97b95302ae4176058b1022ea2efcfaa7ee6dd43
BahaaNimer <bahaanimer97@gmail.com>
2022-07-25 19:04:41 +0300

    need to make the server connect with front

--- src/server.js
+++ src/server.js
@@ -1,19 +1,46 @@
 'use strict';
 
 const express = require('express');
-const signupRoutes=require('./routes/auth_routes/signup.route');
-const verifyRoute=require('./routes/auth_routes/verify.route');
-const signinRoute=require('./routes/auth_routes/signin.route');
-const app = express();
-app.use(express.json());
-app.use('/auth',signupRoutes);
-app.use('/auth',verifyRoute);
-app.use('/auth',signinRoute)
+const cors = require('cors');
+const signupRoutes = require('./routes/auth_routes/signup.route');
+const verifyRoute = require('./routes/auth_routes/verify.route');
+const signinRoute = require('./routes/auth_routes/signin.route');
 const notFound = require('./error/404');
 const errorHandler = require('./error/500');
 
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+app.use(express.json());
+app.use(cors());
+
+
+app.use('/auth', signupRoutes);
+app.use('/auth', verifyRoute);
+app.use('/auth', signinRoute)
+
 app.use('*', notFound);
 app.use(errorHandler);
+
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+})
+
 
 module.exports = {
   server: app,
